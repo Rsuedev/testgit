166a5a7df7718092859f744de45e0f77af074516
fridrikh <alnikf@mail.ru>
2014-09-21 20:43:28 +0400

    First Commit by Fridrikh

--- test/src/com/flash/testgithub/MainActivity.java
+++ test/src/com/flash/testgithub/MainActivity.java
@@ -2,6 +2,7 @@ package com.flash.testgithub;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -9,8 +10,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-    int test = 0;
+        Toast.makeText(getApplicationContext(), "Hello git ;)", Toast.LENGTH_SHORT).show();
+        // nice code :)
 
     }
 }
